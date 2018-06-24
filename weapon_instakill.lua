53821b3424bca31eeb6d580870a8f35263336c55
Dracus Narcrym <dracusnarcrym@gmail.com>
2018-06-25 02:57:33 +0300

    Update weapon_instakill.lua

--- weapon_instakill.lua
+++ weapon_instakill.lua
@@ -2,25 +2,25 @@ SWEP.PrintName		= "InstaKill"
 SWEP.Author			= "Dracus, under the excellent guidance of Fulcrum and code_gs from Facepunch Forums"
 SWEP.Instructions	= "Kills the targeted entity or player by left-clicking"
 
-SWEP.Spawnable				    = true
-SWEP.AdminOnly 				    = true
+SWEP.Spawnable	= true
+SWEP.AdminOnly	= true
 
-SWEP.Primary.ClipSize		  = -1
-SWEP.Primary.DefaultClip	= -1
-SWEP.Primary.Automatic		= true
-SWEP.Primary.Ammo			    = "none"
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = -1
+SWEP.Primary.Automatic = true
+SWEP.Primary.Ammo = "none"
 
-SWEP.Weight					      = 5
-SWEP.AutoSwitchTo			    = false
-SWEP.AutoSwitchFrom		    = false
+SWEP.Weight = 5
+SWEP.AutoSwitchTo = false
+SWEP.AutoSwitchFrom = false
 
-SWEP.Slot				        	= 1
-SWEP.SlotPos		    		  = 2
-SWEP.DrawAmmo			      	= false
-SWEP.DrawCrosshair	  		= true
+SWEP.Slot = 1
+SWEP.SlotPos = 2
+SWEP.DrawAmmo = false
+SWEP.DrawCrosshair = true
 
-SWEP.ViewModel			    	= "models/weapons/v_pistol.mdl"
-SWEP.WorldModel			    	= "models/weapons/w_pistol.mdl"
+SWEP.ViewModel = "models/weapons/v_pistol.mdl"
+SWEP.WorldModel = "models/weapons/w_pistol.mdl"
 
 function SWEP:PrimaryAttack() --This function is run when the player left clicks.
 	if ( CLIENT ) then return end --If the code is being run on the client then don't bother doing this stuff since the client (i.e. players computer) isn't allowed to decide who is dead.
